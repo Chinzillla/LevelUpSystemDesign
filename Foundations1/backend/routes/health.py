09dce67607da398345381254d7f9afba33705950
chinzillla <96362668+Chinzillla@users.noreply.github.com>
2026-05-14 21:18:29 -0400

    Created a health route

--- Foundations1/backend/routes/health.py
+++ Foundations1/backend/routes/health.py
@@ -1,1 +1,7 @@
-# create the health api endpoint
+from flask import Blueprint, jsonify
+
+health_bp = Blueprint('health', __name__, url_prefix='/health')
+
+@health_bp.route('/', methods=['GET'])
+def get_health():
+    return jsonify({'Health': 'I am healthy, Thanks for asking!'})
