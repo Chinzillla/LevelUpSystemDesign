bbe1f19fda2197531a6acfe171ddac3e23d20087
chinzillla <96362668+Chinzillla@users.noreply.github.com>
2026-05-14 21:42:50 -0400

    Created test for the register route

---
+++ Foundations1/backend/tests/route_test.py
@@ -0,0 +1,11 @@
+from app import app
+
+def test_register_route_exists():
+    client = app.test_client()
+
+    response = client.post("/auth/register", json = {
+        "email": "abcd@example.com",
+        "password": "urmum"
+    })
+
+    assert response.status_code == 404
