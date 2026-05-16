deb673ccb73705a6a675c16b662063a5ed6c147f
chinzillla <96362668+Chinzillla@users.noreply.github.com>
2026-05-15 20:45:39 -0400

    created base foundation for item routes

--- Foundations1/backend/routes/items.py
+++ Foundations1/backend/routes/items.py
@@ -1,1 +1,13 @@
-# create the items api endpoint here
+from flask import Blueprint, jsonify, request
+import sqlite3
+from db import get_connection
+
+item_bp = Blueprint('items', __name__, url_prefix='/items')
+
+@item_bp.route('/create', methods=[''])
+
+@item_bp.route('/list', methods=[''])
+
+@item_bp.route('/update', methods=[''])
+
+@item_bp.route('/delete', methods=[''])
